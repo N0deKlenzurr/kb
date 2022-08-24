d9e798a11592fb5b8c3fc8604993133e74299509
Maurice <8485337+N0deKlenzurr@users.noreply.github.com>
2022-08-23 19:52:31 -0500

    Create importan_header_files.cpp

---
+++ importan_header_files.cpp
@@ -0,0 +1,26 @@
+# List of useful and important header files for C/C++
+
+#include <netdb.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <openssl/ssl.h>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <queue>
+#include <dequeue>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <vector>
+#include <cmath>
+#include <cassert>
+#include <unordered_set>
+#include <regex>
+#include <memory>
+#include <Windows.h>
+#include <semaphore.h>
+#include <linux/kernel.h>
+#include <linux/cpu.h>
+# Many included in the linux include directory
+## https://github.com/torvalds/linux/tree/master/include/linux
